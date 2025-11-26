dc46659c49a1a401c18f995ca11f3da337c886a1
zenfyr <git@zenfyr.dev>
2025-11-26 16:00:19 +0700

    icon and itf injection

--- src/main/java/dev/zenfyr/pulsar/resources/DataPackContentsAccessor.java
+++ src/main/java/dev/zenfyr/pulsar/resources/DataPackContentsAccessor.java
@@ -1,8 +1,13 @@
 package dev.zenfyr.pulsar.resources;
 
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.packs.resources.PreparableReloadListener;
 
 public interface DataPackContentsAccessor {
+
+  static DataPackContentsAccessor get(MinecraftServer server) {
+      return ((DataPackContentsAccessor) server);
+  }
 
   <T extends PreparableReloadListener> T pulsar$getReloader(ReloaderType<T> type);
 }
