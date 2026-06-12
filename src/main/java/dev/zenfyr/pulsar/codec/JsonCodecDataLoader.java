dd9f0af961b994ec11a536591a98152dbed588cf
zenfyr <git@zenfyr.dev>
2026-06-12 14:59:55 +0700

    codec: move codec parsing to prepare, expose custom dynamic ops

--- src/main/java/dev/zenfyr/pulsar/codec/JsonCodecDataLoader.java
+++ src/main/java/dev/zenfyr/pulsar/codec/JsonCodecDataLoader.java
@@ -1,12 +1,12 @@
 package dev.zenfyr.pulsar.codec;
 
-import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.JsonOps;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiConsumer;
 import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.minecraft.resources.ResourceLocation;
@@ -32,9 +32,18 @@ public abstract class JsonCodecDataLoader<T> extends SimpleJsonResourceReloadLis
 
   private final ResourceLocation location;
   private final Codec<T> codec;
+  private final DynamicOps<JsonElement> dynamicOps;
+
+  private Map<ResourceLocation, T> temp;
 
   public JsonCodecDataLoader(ResourceLocation location, Codec<T> codec) {
+    this(JsonOps.INSTANCE, location, codec);
+  }
+
+  public JsonCodecDataLoader(
+      DynamicOps<JsonElement> dynamicOps, ResourceLocation location, Codec<T> codec) {
     super(new Gson(), location.toString().replace(':', '/'));
+    this.dynamicOps = dynamicOps;
     this.location = location;
     this.codec = codec;
   }
@@ -45,18 +54,44 @@ public abstract class JsonCodecDataLoader<T> extends SimpleJsonResourceReloadLis
   }
 
   @Override
-  protected void apply(
+  protected Map<ResourceLocation, JsonElement> prepare(
+      ResourceManager resourceManager, ProfilerFiller profilerFiller) {
+    Map<ResourceLocation, T> delegate = new HashMap<>();
+    Map<ResourceLocation, JsonElement> map = new MapFunction<>((location1, jsonElement) -> {
+      T entry = this.codec.parse(this.dynamicOps, jsonElement).getOrThrow(false, string -> {
+        throw new JsonParseException(string);
+      });
+      delegate.put(location1, entry);
+    });
+    scanDirectory(resourceManager, this.directory, this.gson, map);
+    this.temp = delegate;
+    return map;
+  }
+
+  @Override
+  protected final void apply(
       Map<ResourceLocation, JsonElement> prepared,
       ResourceManager manager,
       ProfilerFiller profiler) {
-    this.apply(
-        Maps.transformValues(
-            prepared,
-            input -> this.codec.parse(JsonOps.INSTANCE, input).getOrThrow(false, string -> {
-              throw new JsonParseException(string);
-            })),
-        manager);
+    this.apply(this.temp, manager);
+    this.temp = null;
   }
 
   protected abstract void apply(Map<ResourceLocation, T> parsed, ResourceManager manager);
+
+  static final class MapFunction<K, V> extends HashMap<K, V> {
+
+    private final BiConsumer<K, V> consumer;
+
+    MapFunction(BiConsumer<K, V> consumer) {
+      this.consumer = consumer;
+    }
+
+    @Override
+    public V put(K key, V value) {
+      V val = super.put(key, value);
+      this.consumer.accept(key, value);
+      return val;
+    }
+  }
 }
