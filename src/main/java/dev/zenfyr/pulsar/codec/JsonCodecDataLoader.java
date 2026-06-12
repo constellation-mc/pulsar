9badf62a9e4a5f6fe63a9d45a07f1ddbf72830c5
zenfyr <git@zenfyr.dev>
2026-06-12 14:55:33 +0700

    codec: expose custom dynamic ops

--- src/main/java/dev/zenfyr/pulsar/codec/JsonCodecDataLoader.java
+++ src/main/java/dev/zenfyr/pulsar/codec/JsonCodecDataLoader.java
@@ -1,6 +1,9 @@
 package dev.zenfyr.pulsar.codec;
 
+import com.google.gson.JsonElement;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.JsonOps;
 import java.util.Map;
 import java.util.function.BiConsumer;
 import net.minecraft.resources.FileToIdConverter;
@@ -24,12 +27,17 @@ public abstract class JsonCodecDataLoader<T> extends SimpleJsonResourceReloadLis
     };
   }
 
-  public JsonCodecDataLoader(Identifier location, Codec<T> codec) {
-    super(codec, FileToIdConverter.json(location.toString().replace(':', '/')));
+  public JsonCodecDataLoader(Identifier identifier, Codec<T> codec) {
+    this(JsonOps.INSTANCE, identifier, codec);
+  }
+
+  public JsonCodecDataLoader(
+      DynamicOps<JsonElement> dynamicOps, Identifier identifier, Codec<T> codec) {
+    super(dynamicOps, codec, FileToIdConverter.json(identifier.toString().replace(':', '/')));
   }
 
   @Override
-  protected void apply(
+  protected final void apply(
       Map<Identifier, T> object, ResourceManager resourceManager, ProfilerFiller profilerFiller) {
     this.apply(object, resourceManager);
   }
