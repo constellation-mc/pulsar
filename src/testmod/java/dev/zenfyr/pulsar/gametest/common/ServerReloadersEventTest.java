3cfce1f4e4675b74a0794ad63821a217eb268344
zenfyr <git@zenfyr.dev>
2026-06-27 22:55:03 +0700

    resources: remove fabric specific register method.

--- src/testmod/java/dev/zenfyr/pulsar/gametest/common/ServerReloadersEventTest.java
+++ src/testmod/java/dev/zenfyr/pulsar/gametest/common/ServerReloadersEventTest.java
@@ -6,11 +6,11 @@ import dev.zenfyr.pulsar.gametest.util.Utils;
 import dev.zenfyr.pulsar.impl.PulsarLog;
 import java.util.Objects;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 
 public class ServerReloadersEventTest implements ModInitializer {
 
@@ -20,13 +20,14 @@ public class ServerReloadersEventTest implements ModInitializer {
 
   @Override
   public void onInitialize() {
-    ServerReloadersEvent.EVENT.listen(context -> context.register(new TestReloader(context)));
+    ServerReloadersEvent.EVENT.listen(
+        context -> context.register(TYPE.identifier(), new TestReloader(context)));
 
     // make sure that the reloader triggered
     Utils.addLateCheck("reload listener trigger set", () -> Objects.requireNonNull(trigger));
   }
 
-  public static class TestReloader implements SimpleSynchronousResourceReloadListener {
+  public static class TestReloader implements ResourceManagerReloadListener {
 
     private final ServerReloadersEvent.Context context;
 
@@ -35,22 +36,22 @@ public class ServerReloadersEventTest implements ModInitializer {
     }
 
     @Override
-    public Identifier getFabricId() {
-      return TYPE.identifier();
-    }
-
-    @Override
     public void onResourceManagerReload(ResourceManager manager) {
       trigger = "set";
       PulsarLog.logger()
           .info(
               "reload {}: {}",
-              context.reloader(TYPE).getFabricId(),
+              context.reloader(TYPE),
               context
                   .registryAccess()
                   .lookupOrThrow(Registries.DIMENSION_TYPE)
                   .get(ResourceKey.create(
                       Registries.DIMENSION_TYPE, Identifier.withDefaultNamespace("overworld"))));
+    }
+
+    @Override
+    public String toString() {
+      return TYPE.identifier().toString();
     }
   }
 }
