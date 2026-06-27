03c3b640414951e08eb95d82261423c3e2870c0c
zenfyr <git@zenfyr.dev>
2026-06-27 23:06:35 +0700

    creativetab: rename render method

--- src/main/java/dev/zenfyr/pulsar/impl/mixins/client/creativetab/CreativeModeInventoryScreenMixin.java
+++ src/main/java/dev/zenfyr/pulsar/impl/mixins/client/creativetab/CreativeModeInventoryScreenMixin.java
@@ -46,7 +46,7 @@ public abstract class CreativeModeInventoryScreenMixin
     if (((CreativeModeTabExtensions) group).pulsar$getIconAnimation() != null) {
       ((CreativeModeTabExtensions) group)
           .pulsar$getIconAnimation()
-          .animateIcon(group, context, l, m, bl, bl2);
+          .renderTabIcon(group, context, l, m, bl, bl2);
       ci.cancel();
     }
   }
