f1fbf6807b16213ac541cacf73866a40d46f94f9
zenfyr <git@zenfyr.dev>
2026-06-27 23:10:33 +0700

    fakelevel: move field and init to impl class

--- src/main/java/dev/zenfyr/pulsar/impl/client/PulsarClient.java
+++ src/main/java/dev/zenfyr/pulsar/impl/client/PulsarClient.java
@@ -2,14 +2,14 @@ package dev.zenfyr.pulsar.impl.client;
 
 import dev.zenfyr.pulsar.api.client.events.AfterFirstReload;
 import dev.zenfyr.pulsar.api.client.fakelevel.BrightLightTexture;
-import dev.zenfyr.pulsar.api.client.fakelevel.FakeLevel;
+import dev.zenfyr.pulsar.impl.client.fakelevel.FakeLevelImpl;
 import net.fabricmc.api.ClientModInitializer;
 
 public class PulsarClient implements ClientModInitializer {
 
   @Override
   public void onInitializeClient() {
-    FakeLevel.init();
+    FakeLevelImpl.init();
     AfterFirstReload.EVENT.listen(BrightLightTexture::init);
   }
 }
