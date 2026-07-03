956988dcebb9efa9212449d97d2631a327df3b67
zenfyr <git@zenfyr.dev>
2026-07-04 02:29:45 +0700

    creativetab: 26.1.2

--- src/main/java/dev/zenfyr/pulsar/impl/mixins/creativetab/CreativeModeTabNeoForgeMixin.java
+++ src/main/java/dev/zenfyr/pulsar/impl/mixins/creativetab/CreativeModeTabNeoForgeMixin.java
@@ -3,7 +3,6 @@ package dev.zenfyr.pulsar.impl.mixins.creativetab;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.zenfyr.pulsar.impl.creativetab.CreativeModeTabDuck;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,18 +15,17 @@ public class CreativeModeTabNeoForgeMixin {
           @At(
               value = "INVOKE",
               target =
-                  "Lnet/neoforged/neoforge/event/EventHooks;onCreativeModeTabBuildContents(Lnet/minecraft/world/item/CreativeModeTab;Lnet/minecraft/resources/ResourceKey;Lnet/minecraft/world/item/CreativeModeTab$DisplayItemsGenerator;Lnet/minecraft/world/item/CreativeModeTab$ItemDisplayParameters;Lnet/minecraft/world/item/CreativeModeTab$Output;)V"),
+                  "Lnet/neoforged/neoforge/event/EventHooks;onCreativeModeTabBuildContents(Lnet/minecraft/world/item/CreativeModeTab;Lnet/minecraft/world/item/CreativeModeTab$DisplayItemsGenerator;Lnet/minecraft/world/item/CreativeModeTab$ItemDisplayParameters;Lnet/minecraft/world/item/CreativeModeTab$Output;)V"),
       method = "buildContents",
       require = 0)
   private void pulsar$disableForgeEvent(
       CreativeModeTab tab,
-      ResourceKey<CreativeModeTab> tabKey,
       CreativeModeTab.DisplayItemsGenerator originalGenerator,
       CreativeModeTab.ItemDisplayParameters params,
       CreativeModeTab.Output output,
       Operation<Void> original) {
     if (!((CreativeModeTabDuck) tab).pulsar$isPulsarTab()) {
-      original.call(tab, tabKey, originalGenerator, params, output);
+      original.call(tab, originalGenerator, params, output);
     } else {
       originalGenerator.accept(params, output);
     }
